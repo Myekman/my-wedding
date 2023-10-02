4a5a0335f0becc62948d7a2b74ea5ae347c997c7
Myekman <my@traningstemplet.se>
2023-10-02 08:44:25 +0000

    started project with profiles app and created a superuser

--- mywedding/settings.py
+++ mywedding/settings.py
@@ -21,10 +21,7 @@ os.environ.get('CLOUDINARY_URL')
 }
 
 MEDIA_URL = '/media/'
-
-DEFAULT_FILE_STORAGE =
-'cloudinary_storage.storage.MediaCloudinaryStorage
-'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +36,7 @@ SECRET_KEY = 'django-insecure-!l&pwrkx7#!b8btgkn+$9=hwwdp-x*@5*)*&ked&!#cd55%m#c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '8000-myekman-mywedding-nyh1urjz903.ws-eu105.gitpod.io']
 
 
 # Application definition
@@ -53,6 +50,8 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+
+    'profiles',
 ]
 
 MIDDLEWARE = [
