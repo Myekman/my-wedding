401f7d06eaf29c1c08510ef21ffd48fb4f445890
Myekman <my@traningstemplet.se>
2023-10-04 08:08:53 +0000

    adding extra fields to posts and profiles, likes ans comments_count

--- posts/serializers.py
+++ posts/serializers.py
@@ -9,7 +9,8 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
-
+    likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
 # -----------------------------------------------------------------------------Validate the image feild in Post model
     def validate_image(self, value):
@@ -46,5 +47,5 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'image_filter',
-            'like_id'
+            'like_id', 'comments_count', 'likes_count'
         ]
