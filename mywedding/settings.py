846e60015f1af7fad3524bd2d34547374fa77bb0
Myekman <my@traningstemplet.se>
2023-10-06 09:43:05 +0000

    cors headers bug

--- mywedding/settings.py
+++ mywedding/settings.py
@@ -128,6 +128,29 @@ CORS_ALLOWED_ORIGINS = [
     os.environ.get('CLIENT_ORIGIN')
 ]
 
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 ROOT_URLCONF = 'mywedding.urls'
 
 TEMPLATES = [
