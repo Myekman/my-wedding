013283134531382c027b15e65b95106bc673d9ee
Myekman <my@traningstemplet.se>
2023-10-09 12:09:51 +0000

    installed react-router and code to navigate between pages

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,27 +3,31 @@ import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import Container from "react-bootstrap/Container"
 
+import { NavLink } from 'react-router-dom';
+import styles from "../styles/NavBar.module.css";
 import logo from "../assets/logo.png"
 
 const NavBar = () => {
   return (
-  <Navbar expand="md" fixed="top">
+  <Navbar className={styles.NavBar} expand="md" fixed="top">
     <Container>
-        <Navbar.Brand>
-            <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+        <NavLink to="/">
+            <Navbar.Brand>
+                <img src={logo} alt="logo" height="45" />
+            </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <NavLink to="/">
                 <i className = "fas fa-home"></i>Home
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/signin">
                 <i className = "fas fa-sign-in"></i>Sign in
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/signup">
                 <i className = "fas fa-user-plus"></i>Sign up
-            </Nav.Link>
+            </NavLink>
             </Nav>
         </Navbar.Collapse>
   </Container>
