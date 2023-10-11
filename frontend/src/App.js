696603f54e4d8839723200a63ecdae271bfa942d
Myekman <my@traningstemplet.se>
2023-10-11 09:22:25 +0000

    add code to PostCreateForm to creating a post and upload images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ from './components/NotFound';
 import { Route, Switch } from "react-router-dom";
 import SignUpForm from './pages/auth/SignupForm';
 import SigninForm from './pages/auth/SigninForm';
+import PostCreateForm from './pages/posts/PostCreateForm';
 // import "./api/axiosDefaults";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
               <Route exact path="/" render={() => <h1>Home page</h1>} />
               <Route exact path="/signin" render={() => <SigninForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
+              <Route exact path="/posts/create" render={() => <PostCreateForm />} />
               <Route render = {() => <NotFound/>} />
             </Switch>
           </Container>
